61c8b9530691c862a201ab1e759bf488d3bc2566
Arthur Alekseev <arthur100500@gmail.com>
2024-10-11 03:22:32 +0300

    Tried to add full span based parsing and failed
    Signed-off-by: Arthur Alekseev <arthur100500@gmail.com>

--- MovieProcessor/FileParser.fs
+++ MovieProcessor/FileParser.fs
@@ -32,14 +32,13 @@ module FileParser =
 
     // "tagId,tag"
     // -> tagId, tag
-    let splitTagCodes (line : string) =
-        let [|tagId; tag|] = line.Split(",")
-        {tagId=tagId |> Int32.Parse; tag=tag}
+    let splitTagCodes (line : Span<char>) : obj =
+        let firstSeparatorPlacement = line.IndexOf(',')
+        {tagId=Int32.Parse(line.Slice(0, firstSeparatorPlacement)); tag=line.Slice(firstSeparatorPlacement).ToString()}
         
     // "tconst	ordering	nconst	category	job	characters"
     // -> tconst, nconst, category
-    let splitActorsDirectors (line : string) =
-        let line = line.AsSpan()
+    let splitActorsDirectors (line : Span<char>) : obj =
         let secondSeparatorPlacement = line.Slice(10).IndexOf('\009') + 10
         let thirdSeparatorPlacement = line.Slice(secondSeparatorPlacement + 9).IndexOf('\009') + secondSeparatorPlacement + 9
         let tconst = line.Slice(2, 7)
@@ -50,63 +49,96 @@ module FileParser =
 
     // "nconst	primaryName	birthYear	deathYear	primaryProfession	knownForTitles"
     // -> nconst, primaryName
-    let splitActorsDirectorsNames (line : string) =
-        let line = line.AsSpan()
+    let splitActorsDirectorsNames (line : Span<char>) : obj =
         let secondSeparatorPlacement = line.Slice(12).IndexOf('\009') + 12
         {nconst=Int32.Parse(line.Slice(2, 7))
          primaryName=line.Slice(10, secondSeparatorPlacement - 10).ToString()}
 
     // "tconst	averageRating	numVotes"
     // -> tconst, averageRating
-    let splitRatings (line : string) =
-        let [|tconst; averageRating; _|] = line.Split('\009')
-        {tconst=tconst.Substring(2, 7) |> Int32.Parse
-         averageRating=averageRating |> fun x -> Single.Parse(x, CultureInfo.InvariantCulture)}
+    let splitRatings (line : Span<char>) : obj =
+        let firstSeparatorPlacement = line.IndexOf('\009')
+        let secondSeparatorPlacement = line.Slice(12).IndexOf('\009') + 12
+        {tconst=Int32.Parse(line.Slice(2, 7))
+         averageRating=Single.Parse(line.Slice(firstSeparatorPlacement + 1, secondSeparatorPlacement - firstSeparatorPlacement), CultureInfo.InvariantCulture)}
         
     // "movieId,imdbId,tmdbId"
     // -> movieId, imdbId
-    let splitLinks (line : string) =
-        let [|movieId; imdbId; _|] = line.Split(',')
-        {movieId=movieId |> Int32.Parse
-         imdbId=imdbId |> Int32.Parse}
-        
+    let splitLinks (line : Span<char>) : obj =
+        let firstSeparatorPlacement = line.IndexOf(',')
+        let secondSeparatorPlacement = line.Slice(firstSeparatorPlacement + 1).IndexOf(',') + firstSeparatorPlacement + 1
+        // Here exc is ok!
+        {movieId=Int32.Parse(line.Slice(0, firstSeparatorPlacement))
+         imdbId=Int32.Parse(line.Slice(firstSeparatorPlacement + 1, secondSeparatorPlacement - firstSeparatorPlacement - 1))}
+
     // "titleId	ordering	title	region	language	types	attributes	isOriginalTitle"
     // -> titleId, title, region
-    let splitMovieCodes (line : string) =
-        let [|titleId; _; title; region; _; _; _; _|] = line.Split("\t")
-        {titleId=titleId.Substring(2, 7) |> Int32.Parse
-         title=title
-         region=region}
-        
+    let splitMovieCodes (line : Span<char>) : obj =
+        let firstSeparatorPlacement = line.IndexOf('\009')
+        let secondSeparatorPlacement = line.Slice(firstSeparatorPlacement + 1).IndexOf('\009') + firstSeparatorPlacement + 1
+        let thirdSeparatorPlacement = line.Slice(secondSeparatorPlacement + 1).IndexOf('\009') + secondSeparatorPlacement + 1
+        let fourthSeparatorPlacement = line.Slice(thirdSeparatorPlacement + 1).IndexOf('\009') + thirdSeparatorPlacement + 1
+        // [|titleId; _; title; region; _; _; _; _|]
+        {titleId=Int32.Parse(line.Slice(2, firstSeparatorPlacement - 2))
+         title=line.Slice(secondSeparatorPlacement + 1, thirdSeparatorPlacement - secondSeparatorPlacement).ToString()
+         region=line.Slice(thirdSeparatorPlacement + 1, fourthSeparatorPlacement - thirdSeparatorPlacement).ToString()}
+
     // "movieId,tagId,relevance"
     // -> movieId, tagId, relevance
-    let splitTagScores (line : string) =
-        let line = line.AsSpan()
+    let splitTagScores (line : Span<char>) : obj =
         let firstSplit = line.Slice(1).IndexOf(',') + 1
         let secondSplit = line.Slice(firstSplit + 1).IndexOf(',') + firstSplit + 1
         {movieId=Int32.Parse(line.Slice(0, firstSplit))
          tagId=Int32.Parse(line.Slice(firstSplit + 1, secondSplit - firstSplit - 1))
          relevance=Single.Parse(line.Slice(secondSplit + 1, line.Length - secondSplit - 1), CultureInfo.InvariantCulture)}
 
-    let mutable queuedProcessesCount = 0
+    // parserId:
+    //
+    let commonArray = Array.create<char> 100000 '\000'
 
-    let fileIter parse (path: string) f =
-        let fileReadStream = File.OpenRead path
+    let splitFileUsingSpan (fileReadStream : Stream) parserId k =
         use reader = new StreamReader(fileReadStream)
-        reader.ReadLine() |> ignore
-        let rec inner () =
-            let line = reader.ReadLine()
-            match line with
-            | null -> ()
-            | data ->
-                data |> parse |> f |> ignore
-                inner ()
-        inner ()
+        let allFileSpan = System.Span(commonArray)
+        let mutable length = reader.Read(allFileSpan)
+        let mutable lineStart = 0
+        let mutable index = 0
+        let mutable isHeaderLine = true
+        // TODO: If there is a preprocessor, use preprocessor to generate functions for each file
+        while length = 100000 || isHeaderLine do
+            for c in allFileSpan do
+                match c with
+                | '\n' when isHeaderLine ->
+                    lineStart <- index + 1
+                    isHeaderLine <- false
+                | '\n' ->
+                    let d = (allFileSpan.Slice(lineStart, index - lineStart - 1))
+                    match parserId with
+                    | 0 -> k(splitTagCodes d)
+                    | 1 -> k(splitActorsDirectors d)
+                    | 2 -> k(splitActorsDirectorsNames d)
+                    | 3 -> k(splitRatings d)
+                    | 4 ->
+                        try
+                            k(splitLinks d)
+                        with _ -> ()
+                    | 5 ->
+                        let r = splitMovieCodes d :?> movieCodesRow
+                        if (r.region = "RU" || r.region = "US" || r.region = "EN" || r.region = "AU") then k(r)
+                        else ()
+                    | 6 -> k(splitTagScores d)
+                    | _ -> failwith "Unknown parser ID"
+                    lineStart <- index + 1
+                | _ when index % 100000 = 0 ->
+                    length <- reader.Read(commonArray)
+                    lineStart <- 0
+                    index <- 0
+                | _ -> ()
+                index <- index + 1
 
-    let rec waitForAllTasks () =
-        while queuedProcessesCount <> 0 do
-            printfn "%d" queuedProcessesCount
-            Thread.Yield() |> ignore
+    let fileIter (parserId : int) (path: string) (k : obj -> unit) =
+        let fileReadStream = File.OpenRead(path)
+        splitFileUsingSpan fileReadStream parserId k
+
 
     let rec fileMap parse (path: string) =
         let fileReadStream = File.OpenRead path
--- MovieProcessor/MovieReader.fs
+++ MovieProcessor/MovieReader.fs
@@ -40,42 +40,38 @@ module MovieLoader =
 
         let actorsDirectorsCodes =
             Path.Combine [|path; "ActorsDirectorsCodes_IMDB.tsv"|]
-            |> FileParser.fileIter splitActorsDirectors
+            |> fileIter 1
 
         let actorsDirectorsNames =
             Path.Combine [|path; "ActorsDirectorsNames_IMDB.txt"|]
-            |> FileParser.fileIter splitActorsDirectorsNames
-            
-        let actorsDirectorsCodesCopy =
-            Path.Combine [|path; "ActorsDirectorsCodes_IMDB.tsv"|]
-            |> FileParser.fileMap splitActorsDirectors
+            |> fileIter 2
             
         let ratingsImdb =
             Path.Combine [|path; "Ratings_IMDB.tsv"|]
-            |> FileParser.fileIter splitRatings
+            |> fileIter 3
             
         let linksImdbMovieLens =
             Path.Combine [|path; "links_IMDB_MovieLens.csv"|]
-            |> FileParser.fileIter splitLinks
+            |> fileIter 4
             
         let movieCodes =
             Path.Combine [|path; "MovieCodes_IMDB.tsv"|]
-            |> FileParser.fileMap splitMovieCodes
+            |> fileIter 5
             
         let tagCodes =
             Path.Combine [|path; "TagCodes_MovieLens.csv"|]
-            |> FileParser.fileIter splitTagCodes
+            |> fileIter 0
             
         let tagScores =
             Path.Combine [|path; "TagScores_MovieLens.csv"|]
-            |> FileParser.fileIter splitTagScores
+            |> fileIter 6
             
         // Dictionaries for fast loading
         logger.info "Organizing data into dictionaries"
         logger.info "Tag Codes"
-        let tagCodesDict = dictByField (fun (x : tagCodesRow) -> x.tagId) tagCodes
+        let tagCodesDict = dictByField (fun (x : obj) -> (x :?> tagCodesRow).tagId) tagCodes
         logger.info "Movie ID Links"
-        let imdbOfMl = dictByField (fun (x : linksRow) -> x.movieId) linksImdbMovieLens
+        let imdbOfMl = dictByField (fun (x : obj) -> (x :?> linksRow).movieId) linksImdbMovieLens
 
         // IMDB ID to Movie
         let movies = ConcurrentDictionary<int, Movie>(Environment.ProcessorCount, 980000)
@@ -84,20 +80,18 @@ module MovieLoader =
         // Tags
         let tags = ConcurrentDictionary<string, ConcurrentHashSet<Movie>>(Environment.ProcessorCount, 2000)
 
-        let ruEn = movieCodes |> Seq.filter (fun r -> r.region = "RU" || r.region = "US" || r.region = "GB" || r.region = "AU")
-        let ruEnImdbIds = ConcurrentHashSet<_>()
-
         // Create movie with empty tags and no rating
         logger.info "Loading movies"
-        for row in ruEn do
+        movieCodes (fun row ->
+            let row = row :?> movieCodesRow
             let imdbId = row.titleId
-            ruEnImdbIds.Add imdbId |> ignore
             let movie = Movie(imdbId, row.title, ConcurrentHashSet<Tag>())
-            movies.TryAdd(imdbId, movie) |> ignore
+            movies.TryAdd(imdbId, movie) |> ignore)
 
         // Set rating
         logger.info "Loading movie ratings"
         ratingsImdb (fun row ->
+            let row = row :?> ratingsRow
             let movie = tryGet movies row.tconst
             let rating = row.averageRating |> float32
             movie |> Option.iter (_.SetRating(rating)))
@@ -106,9 +100,10 @@ module MovieLoader =
         // TODO: Filter by imdbId in ru/en
         logger.info "Loading movie tags"
         tagScores (fun row ->
-            let imdbId = row.movieId |> tryGet imdbOfMl |> Option.map (_.imdbId)
+            let row = row :?> tagScoresRow
+            let imdbId = row.movieId |> tryGet imdbOfMl |> Option.map (fun x -> (x :?> linksRow).imdbId)
             let movie = imdbId |> Option.bind (tryGet movies)
-            let tag = tryGet tagCodesDict row.tagId |> Option.map (_.tag)
+            let tag = tryGet tagCodesDict row.tagId |> Option.map (fun x -> (x :?> tagCodesRow).tag)
             let scoreValid = (float32 row.relevance) > 0.5f
             if scoreValid then
                 movie |> Option.iter (fun movie ->
@@ -123,12 +118,14 @@ module MovieLoader =
         // Create people
         logger.info "Loading people"
         actorsDirectorsNames (fun row ->
+            let row = row :?> actorsDirectorsNamesRow
             let person = Person(row.nconst, row.primaryName)
             people.TryAdd(row.nconst, person) |> ignore)
 
         // Link people to movies
         logger.info "Linking people and movies"
         actorsDirectorsCodes (fun row ->
+            let row = row :?> actorsDirectorsRow
             tryGet movies row.tconst |> Option.iter (fun movie ->
             tryGet people row.nconst |> Option.iter (fun person ->
             match row.category with
@@ -142,7 +139,5 @@ module MovieLoader =
 
         let moviesByName = Seq.map (fun (KeyValue(_, movie : Movie)) -> movie.GetTitle(), movie) movies |> dict
         let peopleByName = Seq.map (fun (KeyValue(_, person : Person)) -> person.GetPrimaryName(), person) people |> dict
-
-        waitForAllTasks()
 
         { movies = moviesByName; people = peopleByName; tags = tags; moviesById = movies; peopleById = people}
