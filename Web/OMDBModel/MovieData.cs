d7b96a893f2dbf4dfaabdf0b2253a8d2e2b8015d
Arthur Alekseev <arthur100500@gmail.com>
2024-11-20 21:39:01 +0300

    Improvements to web, add omdb api usage and change css
    Signed-off-by: Arthur Alekseev <arthur100500@gmail.com>

---
+++ Web/OMDBModel/MovieData.cs
@@ -0,0 +1,12 @@
+﻿namespace Web.OMDBModel;
+
+public class MovieData
+{
+    public string Title { get; set; }
+    public string Released { get; set; }
+    public string Runtime { get; set; }
+    public string Plot { get; set; }
+    public string Awards { get; set; }
+    public string Poster { get; set; }
+    public List<Rating> Ratings { get; set; }
+}
---
+++ Web/OMDBModel/Rating.cs
@@ -0,0 +1,7 @@
+﻿namespace Web.OMDBModel;
+
+public class Rating
+{
+    public string Source { get; set; }
+    public string Value { get; set; }
+}
